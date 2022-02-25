753e8abcc94b6f166c038c1ed2318715286f84a8
RaduLupan <github@lupan.ca>
2022-02-25 16:33:39 -0500

    Added locals

--- ecs-ec2/locals.tf
+++ ecs-ec2/locals.tf
@@ -2,6 +2,8 @@ locals {
   vpc_id   = data.aws_subnet.selected.vpc_id
   vpc_cidr = data.aws_vpc.selected.cidr_block
   
+  cluster_name = "${local.common_tags["Project"]}-cluster"
+
   common_tags = {
     Environment = var.environment
     Project     = var.project
