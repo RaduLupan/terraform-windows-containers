aa80c3a5b19c4f382cd68966e2758208851b3792
RaduLupan <github@lupan.ca>
2022-02-25 14:53:48 -0500

    Added security group for the container intance and user data in the launch config

--- ecs-ec2/ec2.tf
+++ ecs-ec2/ec2.tf
@@ -6,11 +6,42 @@ data "template_file" "user_data" {
     ecs_cluster = aws_ecs_cluster.ecs_windows.name
   }
 }
+
+# Security group for the AutoScaling Group launch configuration.
+resource "aws_security_group" "launch_config" {
+  name        = "ecs-instance-sg"
+  description = "Allow inbound traffic"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [local.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "ecs-cluster-instance-sg"
+    Environment = var.environment
+    Project     = var.project
+    Terraform   = true
+  }
+}
+
 # Create launch configuration for the AutoScaling Group
 resource "aws_launch_configuration" "launch_config" {
-  name          = "asg-launch-config"
+  #name          = "asg-launch-config"
   image_id      = var.ami_id
   instance_type = var.instance_type
+
+  user_data       = data.template_file.user_data.rendered
 
   lifecycle {
     create_before_destroy = true
