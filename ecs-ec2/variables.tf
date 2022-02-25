9da66e61e2e1fce61c79c6dbad760c1c23a1f17e
derasys <radu@lupan.ca>
2022-02-25 10:15:02 -0500

    Initial commit

---
+++ ecs-ec2/variables.tf
@@ -0,0 +1,31 @@
+#----------------------------------------------------------------------------
+# REQUIRED PARAMETERS: You must provide a value for each of these parameters.
+#----------------------------------------------------------------------------
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+}
+
+
+#---------------------------------------------------------------
+# OPTIONAL PARAMETERS: These parameters have resonable defaults.
+#---------------------------------------------------------------
+
+variable "environment" {
+  description = "Environment i.e. dev, test, stage, prod"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+  default     = "10.110.0.0/16"
+}
+
+variable "project" {
+  description = "The name of the project"
+  type        = string
+  default     = "ecs-windows-poc"
+}
