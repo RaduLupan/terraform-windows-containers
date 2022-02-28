8a1c18667787396700a9cc83ff1336343ea0d558
RaduLupan <github@lupan.ca>
2022-02-28 08:25:41 -0500

    Added data source for current region

--- ecs-ec2/locals.tf
+++ ecs-ec2/locals.tf
@@ -4,9 +4,14 @@ locals {
   
   cluster_name = "${local.common_tags["Project"]}-cluster"
 
+  region= data.aws_region.current.name
+
   common_tags = {
     Environment = var.environment
     Project     = var.project
     Terraform   = "true"
   }
 }
+
+# Obtain the name of the AWS region configured on the provider.
+data "aws_region" "current" {}
