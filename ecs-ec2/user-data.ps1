cf05c11dec6d3b4a777662514f3224eefb6b10a4
RaduLupan <github@lupan.ca>
2022-03-12 14:04:03 -0500

    Added EnableTaskENI and AwsvpcBlockIMDS params

--- ecs-ec2/user-data.ps1
+++ ecs-ec2/user-data.ps1
@@ -1,5 +1,5 @@
 <powershell>
 $ECSCluster = "${ecs_cluster}"
 Import-Module ECSTools
-Initialize-ECSAgent -Cluster $ECSCluster -EnableTaskIAMRole -LoggingDrivers '["json-file","awslogs"]'
+Initialize-ECSAgent -Cluster $ECSCluster -EnableTaskIAMRole -LoggingDrivers '["json-file","awslogs"]' -EnableTaskENI -AwsvpcBlockIMDS
 </powershell>
